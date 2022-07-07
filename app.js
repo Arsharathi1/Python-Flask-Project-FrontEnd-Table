483cc7592f7b7813309fe3ee910d2b1eacfc2341
Arsharathi1 <57323283+Arsharathi1@users.noreply.github.com>
2022-07-07 11:10:39 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,394 @@
+var myBooks = [
+    {           "Cage Code":"7Y5Z1",
+                "Company Name": "TESLA AVIONICS, LLC",
+                "Address 1": "6065 NW 167 ST STE B12",
+                "Address 2": null,
+                 "City":"MIAMI",
+                 "State":"FL",
+                 "Zip":"33015-4344",
+                 "Country":"UNITED STATES",
+                 "Date Established":"15-SEP-2017",
+                 "Last Updated":"19-NOV-2019",
+                 "Former Name 1":"Former Name 1",
+                 "Status":"A",
+                 "Type":"F",
+                 "Business Size":"A",
+                 "Woman Owned (Y\/N)":"Y"
+
+    },
+    {
+                "Cage Code":"0194G",
+                "Company Name": "TESLA BLATNA, A.S.",
+                "Address 1": "PALACKEHO 644",
+                "Address 2": null,
+                 "City":"BLATNA",
+                 "State":null,
+                 "Zip":"38801",
+                 "Country":"CZECH REPUBLIC",
+                 "Date Established":"15-AUG-2003",
+                 "Last Updated":"26-NOV-2019",
+                 "Former Name 1":null,
+                 "Status":"A",
+                 "Type":"E",
+                 "Business Size":"N",
+                 "Woman Owned (Y\/N)":"N"
+    },
+    {
+        "Cage Code":"0P8B7",
+        "Company Name": "TESLA BLATNA, A.S.",
+        "Address 1": "PALACKEHO 644",
+        "Address 2": null,
+         "City":"BLATNA",
+         "State":null,
+         "Zip":"38801",
+         "Country":"CZECH REPUBLIC",
+         "Date Established":"15-AUG-2003",
+         "Last Updated":"26-NOV-2019",
+         "Former Name 1":null,
+         "Status":"A",
+         "Type":"E",
+         "Business Size":"N",
+         "Woman Owned (Y\/N)":"N"   
+        
+    },
+    {
+        "Cage Code":"4NPQ9",
+        "Company Name": "TESLA COMMUNICATIONS",
+        "Address 1": "1840 NORTH MELBORN",
+        "Address 2": null,
+         "City":"DEARBORN",
+         "State":"MI",
+         "Zip":"48128",
+         "Country":"UNITED STATES",
+         "Date Established":"13-FEB-2007",
+         "Last Updated":"13-FEB-2007",
+         "Former Name 1":null,
+         "Status":"A",
+         "Type":"F",
+         "Business Size":"A",
+         "Woman Owned (Y\/N)":"N"   
+        
+    },
+    {
+        "Cage Code":"4NPQ9",
+        "Company Name": "TESLA COMMUNICATIONS",
+        "Address 1": "1840 NORTH MELBORN",
+        "Address 2": null,
+         "City":"DEARBORN",
+         "State":"MI",
+         "Zip":"48128",
+         "Country":"UNITED STATES",
+         "Date Established":"13-FEB-2007",
+         "Last Updated":"13-FEB-2007",
+         "Former Name 1":null,
+         "Status":"A",
+         "Type":"F",
+         "Business Size":"A",
+         "Woman Owned (Y\/N)":"N"   
+        
+    },
+    {
+        "Cage Code":"6CZ43",
+        "Company Name": "TESLA DESIGNS LLC",
+        "Address 1": "15010 HERONGLEN DR",
+        "Address 2": null,
+         "City":"LITHIA",
+         "State":"FL",
+         "Zip":"33547-5853",
+         "Country":"UNITED STATES",
+         "Date Established":"29-APR-2011",
+         "Last Updated":"16-MAY-2019",
+         "Former Name 1":null,
+         "Status":"A",
+         "Type":"F",
+         "Business Size":"A",
+         "Woman Owned (Y\/N)":"N"   
+        
+    },
+    {
+        "Cage Code":"6CZ43",
+        "Company Name": "TESLA DESIGNS LLC",
+        "Address 1": "15010 HERONGLEN DR",
+        "Address 2": null,
+         "City":"LITHIA",
+         "State":"FL",
+         "Zip":"33547-5853",
+         "Country":"UNITED STATES",
+         "Date Established":"29-APR-2011",
+         "Last Updated":"16-MAY-2019",
+         "Former Name 1":null,
+         "Status":"A",
+         "Type":"F",
+         "Business Size":"A",
+         "Woman Owned (Y\/N)":"N"   
+        
+    },
+    {
+        "Cage Code":"5HFT9",
+        "Company Name": "TESLA DYNAMICS CORPORATION.",
+        "Address 1": "7285 IDYLLWILD",
+        "Address 2": null,
+         "City":"RIVERSIDE",
+         "State":"CA",
+         "Zip":"92503-7701",
+         "Country":"UNITED STATES",
+         "Date Established":"04-JUN-2009",
+         "Last Updated":"05-MAY-2010",
+         "Former Name 1":null,
+         "Status":"A",
+         "Type":"F",
+         "Business Size":"A",
+         "Woman Owned (Y\/N)":"N"   
+        
+    },
+    {
+        "Cage Code":"7X1U4",
+        "Company Name": "TESLA DYNAMICS LLC",
+        "Address 1": "7285 IDYLLWILD",
+        "Address 2": null,
+         "City":"GLENELG",
+         "State":"MD",
+         "Zip":"21737-9628",
+         "Country":"UNITED STATES",
+         "Date Established":"04-AUG-2017",
+         "Last Updated":"19-JUL-2018",
+         "Former Name 1":null,
+         "Status":"A",
+         "Type":"F",
+         "Business Size":"A",
+         "Woman Owned (Y\/N)":"Y"   
+        
+    },
+    {
+        "Cage Code":"05080",
+        "Company Name": "TESLA ELECTRIC CO",
+        "Address 1": "2605 W ARMITAGE AVE",
+        "Address 2": null,
+         "City":"CHICAGO",
+         "State":"IL",
+         "Zip":"60647-4208",
+         "Country":"UNITED STATES",
+         "Date Established":"25-OCT-1974",
+         "Last Updated":"01-JAN-1961",
+         "Former Name 1":null,
+         "Status":"A",
+         "Type":"F",
+         "Business Size":"null",
+         "Woman Owned (Y\/N)":"null"   
+        
+    },
+    {
+        "Cage Code":"05080",
+        "Company Name": "TESLA ELECTRIC CO",
+        "Address 1": "2605 W ARMITAGE AVE",
+        "Address 2": null,
+         "City":"CHICAGO",
+         "State":"IL",
+         "Zip":"60647-4208",
+         "Country":"UNITED STATES",
+         "Date Established":"25-OCT-1974",
+         "Last Updated":"01-JAN-1961",
+         "Former Name 1":null,
+         "Status":"A",
+         "Type":"F",
+         "Business Size":"null",
+         "Woman Owned (Y\/N)":"null"   
+        
+    },
+    {
+        "Cage Code":"36EK8",
+        "Company Name": "TESLA ELECTRIC, ARMATURE AND MACHINE, INC.",
+        "Address 1": "735 LANE AVE NORTH",
+        "Address 2": null,
+         "City":"JACKSONVILLE",
+         "State":"FL",
+         "Zip":"32254-2824",
+         "Country":"UNITED STATES",
+         "Date Established":"14-FEB-2005",
+         "Last Updated":"26-APR-2016",
+         "Former Name 1":null,
+         "Status":"A",
+         "Type":"F",
+         "Business Size":"A",
+         "Woman Owned (Y\/N)":"N"   
+        
+    },
+    {
+        "Cage Code":"57K43",
+        "Company Name": "TESLA ELECTRIC, LLC DBA TESLA INSTRUMENTS",
+        "Address 1": "1760 ABBOTT RD",
+        "Address 2": null,
+         "City":"ANCHORAGE",
+         "State":"AK",
+         "Zip":"99507-3443",
+         "Country":"UNITED STATES",
+         "Date Established":"29-SEP-2008",
+         "Last Updated":"13-AUG-2019",
+         "Former Name 1":null,
+         "Status":"A",
+         "Type":"F",
+         "Business Size":"A",
+         "Woman Owned (Y\/N)":"N"   
+        
+    },
+    {
+        "Cage Code":"6Z9T8",
+        "Company Name": "TESLA ELECTRICAL SERVICES, LLC",
+        "Address 1": "2500 E TC JESTER BLVD STE 180",
+        "Address 2": null,
+         "City":"HOUSTON",
+         "State":"TX",
+         "Zip":"77008-1464",
+         "Country":"UNITED STATES",
+         "Date Established":"19-SEP-2013",
+         "Last Updated":"28-AUG-2014",
+         "Former Name 1":null,
+         "Status":"A",
+         "Type":"F",
+         "Business Size":"A",
+         "Woman Owned (Y\/N)":"N"   
+        
+    },
+    {
+        "Cage Code":"6R3Y4",
+        "Company Name": "TESLA ELECTRONICS INC.",
+        "Address 1": "2500 E TC JESTER BLVD STE 180",
+        "Address 2": null,
+         "City":"HOUSTON",
+         "State":"CA",
+         "Zip":"90272",
+         "Country":"UNITED STATES",
+         "Date Established":"16-MAY-2012",
+         "Last Updated":"16-MAY-2012",
+         "Former Name 1":null,
+         "Status":"A",
+         "Type":"A",
+         "Business Size":"N",
+         "Woman Owned (Y\/N)":"N"   
+        
+    },
+    {
+        "Cage Code":"3844G",
+        "Company Name": "TESLA ELECTRONTUBES S.R.O.",
+        "Address 1": "KOLOVRATSKA 1445",
+        "Address 2": null,
+         "City":"RICANY",
+         "State":"null",
+         "Zip":"25101",
+         "Country":"CZECH REPUBLIC",
+         "Date Established":"23-OCT-2007",
+         "Last Updated":"26-NOV-2019",
+         "Former Name 1":null,
+         "Status":"A",
+         "Type":"E",
+         "Business Size":"N",
+         "Woman Owned (Y\/N)":"N"   
+        
+    },
+    {
+        "Cage Code":"T7262",
+        "Company Name": "TESLA ELEKTRONIK SAN. VE TIC. LTD. STI.",
+        "Address 1": "UGUR MUMCU SOKAK. NO:17\/3",
+        "Address 2": null,
+         "City":"ANKARA\/GAZIOSMANPASA",
+         "State":"null",
+         "Zip":"06700",
+         "Country":"TURKEY",
+         "Date Established":"04-DEC-2006",
+         "Last Updated":"07-JAN-2019",
+         "Former Name 1":null,
+         "Status":"A",
+         "Type":"F",
+         "Business Size":"N",
+         "Woman Owned (Y\/N)":"N"   
+        
+    },
+    
+    {
+        "Cage Code":"0175G",
+        "Company Name": "TESLA ELMI, A.S.",
+        "Address 1": "UGUR MUMCU SOKAK. NO:17\/3",
+        "Address 2": null,
+         "City":"ANKARA\/GAZIOSMANPASA",
+         "State":"null",
+         "Zip":"06700",
+         "Country":"CZECH REPUBLIC",
+         "Date Established":"01-NOV-2009",
+         "Last Updated":"26-NOV-2019",
+         "Former Name 1":null,
+         "Status":"A",
+         "Type":"E",
+         "Business Size":"N",
+         "Woman Owned (Y\/N)":"N"   
+        
+    },
+    {
+        "Cage Code":"6ELS9",
+        "Company Name": "TESLA ENERGY PARTNERS, LLC",
+        "Address 1": "2711 JEFFERSON DAVIS HWY STE 200",
+        "Address 2": null,
+         "City":"ARLINGTON",
+         "State":"VA",
+         "Zip":"22202-4015",
+         "Country":"UNITED STATES",
+         "Date Established":"14-SEP-2012",
+         "Last Updated":"20-JAN-2018",
+         "Former Name 1":null,
+         "Status":"A",
+         "Type":"F",
+         "Business Size":"A",
+         "Woman Owned (Y\/N)":"N"   
+        
+    },
+    
+    
+    
+    
+    
+    
+
+    
+    
+]
+
+
+// EXTRACT VALUE FOR HTML HEADER. 
+// ('Book ID', 'Book Name', 'Category' and 'Price')
+var col = [];
+for (var i = 0; i < myBooks.length; i++) {
+    for (var key in myBooks[i]) {
+        if (col.indexOf(key) === -1) {
+            col.push(key);
+        }
+    }
+}
+
+// CREATE DYNAMIC TABLE.
+var table = document.createElement("table");
+
+// CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+var tr = table.insertRow(-1);                   // TABLE ROW.
+
+for (var i = 0; i < col.length; i++) {
+    var th = document.createElement("th");      // TABLE HEADER.
+    th.innerHTML = col[i];
+    tr.appendChild(th);
+}
+
+// ADD JSON DATA TO THE TABLE AS ROWS.
+for (var i = 0; i < myBooks.length; i++) {
+
+    tr = table.insertRow(-1);
+
+    for (var j = 0; j < col.length; j++) {
+        var tabCell = tr.insertCell(-1);
+        tabCell.innerHTML = myBooks[i][col[j]];
+    }
+}
+
+// FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+var divContainer = document.getElementById("showData")
+divContainer.innerHTML = "";
+divContainer.appendChild(table);
+
